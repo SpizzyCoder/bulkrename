8e1f00228cedc2501f9dd255ef11d55b39c6c0d1
SpizzyCoder <spizzycoder@protonmail.com>
2022-05-07 01:14:09 +0200

    Made use of iterator functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs,env};
-use std::fs::ReadDir;
+use std::path::PathBuf;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -15,37 +15,35 @@ fn main() {
   let search_str: &str = &args[1];
   let replace_str: &str = &args[2];
 
-  // Get an iterator from current dir
-  let current_dir_iterator: ReadDir = match fs::read_dir(".") {
-    Ok(iterator) => iterator,
+  let objects_as_strings = match fs::read_dir(".") {
+    Ok(iterator) => iterator.filter_map(|x| x.ok()) // Get all the valid items
+                            .filter_map(|x| path_to_string(x.path())),
     Err(error) => {
-      eprintln!["Couldn't open the current dir [Error: {}]",error];
+      eprintln!["Failed to open the current dir [Error: {}]",error];
       return
     }
   };
 
-  for object in current_dir_iterator {
-    // Get the current object as string
-    let current_object: String = match object {
-      Ok(object_entry) => object_entry.path().display().to_string(),
-      Err(error) => {
-        eprintln!["Couldn't get object as string [Error: {}]",error];
-        continue
-      }
-    };
-
-    // Replace substrings in string
-    let renamed_object: String = current_object.replace(search_str,replace_str);
+  for cur_object in objects_as_strings {
+    let renamed_object: String = cur_object.replace(search_str,replace_str);
 
     // Ignore not renamed objects
-    if renamed_object == current_object {
+    if renamed_object == cur_object {
       continue
     }
 
     // Rename the object
-    match fs::rename(&current_object,&renamed_object) {
-      Ok(_) => println!["{} -> {}",current_object,renamed_object],
-      Err(error) => eprintln!["ERROR {} -> {} [Error: {}]",current_object,renamed_object,error]
+    match fs::rename(&cur_object,&renamed_object) {
+      Ok(_) => println!["{} -> {}",cur_object,renamed_object],
+      Err(error) => eprintln!["ERROR {} -> {} [Error: {}]",cur_object,renamed_object,error]
     };
+  }
+}
+
+fn path_to_string(path: PathBuf) -> Option<String> {
+  if path.to_str().is_none() {
+    return None
+  } else {
+    return Some(path.to_str().unwrap().to_string())
   }
 }
